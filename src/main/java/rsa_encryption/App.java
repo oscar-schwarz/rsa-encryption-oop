892c68e5ade844e4d78a6b220b4ded0ccc23d6bc
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-05-13 13:56:58 +0200

    Initial commit

---
+++ src/main/java/rsa_encryption/App.java
@@ -0,0 +1,13 @@
+package rsa_encryption;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
