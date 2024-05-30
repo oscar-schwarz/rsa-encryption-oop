86b443218bac800046db23a5cb5b73ee9ae80e02
Oscar Schwarz <schwarz.oscar@protonmail.com>
2024-05-30 18:34:40 +0200

    fix(Main): smaller primes in sample key pair

--- src/main/java/rsa_encryption/Main.java
+++ src/main/java/rsa_encryption/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main( String[] args )
     {
-        KeyPair keyPair = new KeyPair(359631251336060026838154987799, 752326558075014716271358218791);
+        KeyPair keyPair = new KeyPair(356409787, 356409787);
     }
 }
