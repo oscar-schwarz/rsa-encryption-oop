2ec3aaf55439bdb43da533842b9f15bdc60e7028
Oscar Schwarz <schwarz.oscar@protonmail.com>
2024-05-31 13:04:26 +0200

    feat(Main): initialize instances

--- src/main/java/rsa_encryption/Main.java
+++ src/main/java/rsa_encryption/Main.java
@@ -4,6 +4,26 @@ public class Main
 {
     public static void main( String[] args )
     {
-        KeyPair keyPair = new KeyPair(356409787, 356409787);
+        KeyGenerator generatorA = new KeyGenerator(11, 17);
+        generatorA.generateKeys();
+
+        KeyGenerator generatorB = new KeyGenerator(98963, 49927);
+        generatorB.generateKeys();
+
+        ChatParticipant alice = new ChatParticipant("Alice", generatorA.getKeyPair());
+        ChatParticipant bob = new ChatParticipant("Bob", generatorB.getKeyPair());
+        
+        ChatParticipant.makePartners(alice, bob);
+
+        CryptoFileString gedicht = new CryptoFileString();
+        gedicht.setFilepath("text/Gedicht.txt");
+
+        CryptoFileString hallowelt = new CryptoFileString();
+        hallowelt.setFilepath("text/hallowelt.txt");
+
+        CryptoFileString tragedy = new CryptoFileString();
+        tragedy.setFilepath("text/tragedy.txt");
+
+        bob.sendMessage(hallowelt);
     }
 }
