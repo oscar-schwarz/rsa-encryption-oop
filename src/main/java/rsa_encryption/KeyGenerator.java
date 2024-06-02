e5e741026c9416a488d4bea03119f279ac3c7c26
Eulehund99 <hazemrik@gmail.com>
2024-06-02 23:40:33 +0200

    Added Javadoc comments to KeyPair and KeyGenerator

--- src/main/java/rsa_encryption/KeyGenerator.java
+++ src/main/java/rsa_encryption/KeyGenerator.java
@@ -2,25 +2,54 @@ package rsa_encryption;
 
 import java.math.BigInteger;
 
+/**
+ * The {@code KeyGenerator} class is responsible for generating RSA key pairs.
+ */
 public class KeyGenerator {
     private KeyPair keys;
 
+    /**
+     * Constructs a {@code KeyGenerator} with specified prime numbers {@code p} and {@code q}.
+     *
+     * @param p the first prime number.
+     * @param q the second prime number.
+     */
     public KeyGenerator(BigInteger p, BigInteger q) {
         keys = new KeyPair(p, q);
     }
 
+    /**
+     * Constructs a {@code KeyGenerator} with specified prime numbers {@code p} and {@code q}.
+     *
+     * @param p the first prime number.
+     * @param q the second prime number.
+     */
     public KeyGenerator(int p, int q) {
         keys = new KeyPair(BigInteger.valueOf(p), BigInteger.valueOf(q));
     }
+
+    /**
+     * Generates the RSA public and private keys.
+     */
     public void generateKeys() {
         keys.generatePublicKey();
         keys.generatePrivateKey();
     }
 
+    /**
+     * Retrieves the generated key pair.
+     *
+     * @return the key pair as a {@code KeyPair}.
+     */
     public KeyPair getKeyPair() {
         return keys;
     }
 
+    /**
+     * Returns a string representation of the key pair.
+     *
+     * @return a string representation of the public and private keys.
+     */
     public String toString() {
         String g = keys.getPublicKey().getGeneratorNumber().toString();
         String e = keys.getPublicKey().getKey().toString();
--- src/main/java/rsa_encryption/KeyPair.java
+++ src/main/java/rsa_encryption/KeyPair.java
@@ -2,27 +2,48 @@ package rsa_encryption;
 
 import java.math.BigInteger;
 
+/**
+ * The {@code KeyPair} class represents a pair of RSA keys, consisting of a public key and a private key.
+ */
 public class KeyPair {
     private PublicKey publicKey;
     private PrivateKey privateKey;
 
+    /**
+     * Constructs a {@code KeyPair} with specified prime numbers {@code p} and {@code q}.
+     *
+     * @param p the first prime number.
+     * @param q the second prime number.
+     */
     public KeyPair(BigInteger p, BigInteger q){
         publicKey = new PublicKey(p,q);
         privateKey = new PrivateKey(p,q);
     }
-
+    /**
+     * Generates the private key.
+     */
     public void generatePrivateKey() {
         privateKey.generateKey();
     }
-
+    /**
+     * Generates the public key.
+     */
     public void generatePublicKey() {
         publicKey.generateKey();
     }
-
+    /**
+     * Retrieves the public key.
+     *
+     * @return the public key as a {@code PublicKey}.
+     */
     public PublicKey getPublicKey(){
         return publicKey;
     }
-
+    /**
+     * Retrieves the private key.
+     *
+     * @return the private key as a {@code PrivateKey}.
+     */
     public PrivateKey getPrivateKey(){
         return privateKey;
     }
